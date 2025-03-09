fa537cabfa14571bea898ee865f40539ccb0085f
Govind244 <govindsaraswat244@gmail.com>
2025-03-09 18:27:42 +0530

    Lombok and database settings added

--- src/main/java/com/bridgelabz/AddressBook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/AddressBook/dto/AddressBookDTO.java
@@ -1,21 +1,36 @@
+//package com.bridgelabz.AddressBook.dto;
+//
+//public class AddressBookDTO {
+//
+//    private String name;
+//    private String phoneNumber;
+//    private String email;
+//    private String address;
+//
+//    public AddressBookDTO(String name, String phoneNumber, String email, String address) {
+//        this.name = name;
+//        this.phoneNumber = phoneNumber;
+//        this.email = email;
+//        this.address = address;
+//    }
+//
+//    public String getName() { return name; }
+//    public String getPhoneNumber() { return phoneNumber; }
+//    public String getEmail() { return email; }
+//    public String getAddress() { return address; }
+//}
+
+
 package com.bridgelabz.AddressBook.dto;
 
-public class AddressBookDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class AddressBookDTO {
     private String name;
     private String phoneNumber;
     private String email;
     private String address;
-
-    public AddressBookDTO(String name, String phoneNumber, String email, String address) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
-    }
-
-    public String getName() { return name; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public String getEmail() { return email; }
-    public String getAddress() { return address; }
 }
